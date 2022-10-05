069d0433c6043eef2c4f4cae23ef323ca3584fd5
joseadolfomartinezolvera <56425103+joseadolfomartinezolvera@users.noreply.github.com>
2022-10-04 19:24:28 -0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //variables privadas
-    private float velocidad = 20;
-    private float turnSpeed = 25.0f; 
+    private float velocidad = 20;       //variable que mide la velocidad 
+    private float turnSpeed = 25.0f;    //variable que mide la velocidad de giro
     private float horizontalInput;
     private float forwardInput;
 
@@ -22,11 +22,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");  //hace saber al juego que se controla de izquierda a derecha segun las teclas presionadas
+        forwardInput = Input.GetAxis("Vertical");       //hace saber al juego que se controla de adelante a atras segun las teclas presionadas
 
-
+        // Mueve el automóvil hacia adelante según la entrada vertical
         transform.Translate(Vector3.forward * Time.deltaTime*velocidad * forwardInput);
+        // Mueve el automovil de forma horizontal
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
 
